b4258b62e682c12ca34f003fa6b2bbd9f410199d
Peter Mansell <peter@pmansell.com>
2020-03-23 22:46:41 +0000

    Removing new line

--- src/actions/artists.ts
+++ src/actions/artists.ts
@@ -4,12 +4,12 @@ export const FETCH_ARTISTS = 'FETCH_ARTISTS';
 
 export const fetchArtists = (query: string) => async (dispatch: any) => {
     
-    const data = await NetworkService.getArtist(query);
-    const { resultsPage : { results: { artist } } } = data;
-    console.log(artist);
+  const data = await NetworkService.getArtist(query);
+  const { resultsPage : { results: { artist } } } = data;
+  console.log(artist);
 
-    dispatch({
-        type: FETCH_ARTISTS,
-        payload: artist
-    });
+  dispatch({
+      type: FETCH_ARTISTS,
+      payload: artist
+  });
 }
